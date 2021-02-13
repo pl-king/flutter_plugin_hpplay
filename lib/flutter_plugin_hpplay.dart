e736d8e85f9871a48accaedcd187b71ebd37da87
pl <panglongking@gmail.com>
2021-02-13 11:47:40 +0800

    Android 运行和打包没有问题 ios 运行没有问题 打包没有开发者账户还不能试（第一次运行 在terminal 切换到ios 目录 执行pop install才成功）

--- lib/flutter_plugin_hpplay.dart
+++ lib/flutter_plugin_hpplay.dart
@@ -1,96 +1,96 @@
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class FlutterPluginHpplay {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_plugin_hpplay');
-
-  static final int TYPE_AUDIO = 101;
-  static final int TYPE_VIDEO = 102;
-  static final int TYPE_IMAGE = 103;
-
-  StreamSubscription<dynamic> _streamSubscription;
-
-  static Future<String> get HpplayCloseConnect async {
-    await _channel.invokeMethod('HpplayCloseConnect');
-  }
-
-  static Future<String> get HpplayGetConnectInfos async {
-    await _channel.invokeMethod('HpplayGetConnectInfos');
-  }
-
-  static Future<String> get getNetWorkName async {
-    final String version = await _channel.invokeMethod('getNetWorkName');
-    return version;
-  }
-
-  static Future<String> get initHpplay async {
-    await _channel.invokeMethod('initHpplay');
-  }
-
-  static Future<String> get HpplayBrowse async {
-    final String version = await _channel.invokeMethod('HpplayBrowse');
-    return version;
-  }
-
-  static Future HpplayConnect(String ip) async {
-    Map map = new Map();
-    map['LelinkServiceInfoJson'] = ip;
-    final String version = await _channel.invokeMethod('HpplayConnect', map);
-  }
-
-  void initEvent(eventListener) {
-    print("init event");
-    _streamSubscription = _eventChannelFor()
-        .receiveBroadcastStream()
-        .listen(eventListener, onError: errorListener);
-  }
-
-  void errorListener(dynamic event) {}
-
-  EventChannel _eventChannelFor() {
-    return EventChannel('flutter_plugin_event_discover_device');
-  }
-
-  static Future<void> HpplayPlayVideo(String url) async {
-    Map map = new Map();
-    map['MediaUrl'] = url;
-    map['MediaType'] = TYPE_VIDEO;
-    final String version = await _channel.invokeMethod('HpplayPlay', map);
-  }
-
-  static Future<void> HpplayPlayAudio(String url) async {
-    Map map = new Map();
-    map['MediaUrl'] = url;
-    map['MediaType'] = TYPE_AUDIO;
-    final String version = await _channel.invokeMethod('HpplayPlay', map);
-  }
-
-  static Future<void> HpplayPlayImage(String url) async {
-    Map map = new Map();
-    map['MediaUrl'] = url;
-    map['MediaType'] = TYPE_IMAGE;
-    final String version = await _channel.invokeMethod('HpplayPlay', map);
-  }
-
-  static Future<void> get HpplayPause async {
-    final String version = await _channel.invokeMethod('HpplayPause');
-  }
-
-  static Future<void> get HppalyResume async {
-    final String version = await _channel.invokeMethod('HppalyResume');
-  }
-
-  static Future<void> get HpplayStop async {
-    final String version = await _channel.invokeMethod('HpplayStop');
-  }
-
-  static Future<void> get HpplayVoulumeUp async {
-    final String version = await _channel.invokeMethod('HpplayVoulumeUp');
-  }
-
-  static Future<void> get HpplayVoulumeDown async {
-    final String version = await _channel.invokeMethod('HpplayVoulumeDown');
-  }
-}
+// import 'dart:async';
+//
+// import 'package:flutter/services.dart';
+//
+// class FlutterPluginHpplay {
+//   static const MethodChannel _channel =
+//       const MethodChannel('flutter_plugin_hpplay');
+//
+//   static final int TYPE_AUDIO = 101;
+//   static final int TYPE_VIDEO = 102;
+//   static final int TYPE_IMAGE = 103;
+//
+//   StreamSubscription<dynamic> _streamSubscription;
+//
+//   static Future<String> get HpplayCloseConnect async {
+//     await _channel.invokeMethod('HpplayCloseConnect');
+//   }HpplayCloseConnect
+//
+//   static Future<String> get HpplayGetConnectInfos async {
+//     await _channel.invokeMethod('HpplayGetConnectInfos');
+//   }
+//
+//   static Future<String> get getNetWorkName async {
+//     final String version = await _channel.invokeMethod('getNetWorkName');
+//     return version;
+//   }
+//
+//   static Future<String> get initHpplay async {
+//     await _channel.invokeMethod('initHpplay');
+//   }
+//
+//   static Future<String> get HpplayBrowse async {
+//     final String version = await _channel.invokeMethod('HpplayBrowse');
+//     return version;
+//   }
+//
+//   static Future HpplayConnect(String ip) async {
+//     Map map = new Map();
+//     map['LelinkServiceInfoJson'] = ip;
+//     final String version = await _channel.invokeMethod('HpplayConnect', map);
+//   }
+//
+//   void initEvent(eventListener) {
+//     print("init event");
+//     _streamSubscription = _eventChannelFor()
+//         .receiveBroadcastStream()
+//         .listen(eventListener, onError: errorListener);
+//   }
+//
+//   void errorListener(dynamic event) {}
+//
+//   EventChannel _eventChannelFor() {
+//     return EventChannel('flutter_plugin_event_discover_device');
+//   }
+//
+//   static Future<void> HpplayPlayVideo(String url) async {
+//     Map map = new Map();
+//     map['MediaUrl'] = url;
+//     map['MediaType'] = TYPE_VIDEO;
+//     final String version = await _channel.invokeMethod('HpplayPlay', map);
+//   }
+//
+//   static Future<void> HpplayPlayAudio(String url) async {
+//     Map map = new Map();
+//     map['MediaUrl'] = url;
+//     map['MediaType'] = TYPE_AUDIO;
+//     final String version = await _channel.invokeMethod('HpplayPlay', map);
+//   }
+//
+//   static Future<void> HpplayPlayImage(String url) async {
+//     Map map = new Map();
+//     map['MediaUrl'] = url;
+//     map['MediaType'] = TYPE_IMAGE;
+//     final String version = await _channel.invokeMethod('HpplayPlay', map);
+//   }
+//
+//   static Future<void> get HpplayPause async {
+//     final String version = await _channel.invokeMethod('HpplayPause');
+//   }
+//
+//   static Future<void> get HppalyResume async {
+//     final String version = await _channel.invokeMethod('HppalyResume');
+//   }
+//
+//   static Future<void> get HpplayStop async {
+//     final String version = await _channel.invokeMethod('HpplayStop');
+//   }
+//
+//   static Future<void> get HpplayVoulumeUp async {
+//     final String version = await _channel.invokeMethod('HpplayVoulumeUp');
+//   }
+//
+//   static Future<void> get HpplayVoulumeDown async {
+//     final String version = await _channel.invokeMethod('HpplayVoulumeDown');
+//   }
+// }
--- test/flutter_plugin_hpplay_test.dart
+++ test/flutter_plugin_hpplay_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_plugin_hpplay/flutter_plugin_hpplay.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_plugin_hpplay');
@@ -18,6 +17,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await FlutterPluginHpplay.initHpplay, '42');
+    // expect(await Flutterpluginhpplay.initHpplay, '42');
   });
 }
